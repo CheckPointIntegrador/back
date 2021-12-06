22be783af052c1e43b5911fd61fb858dc61a4530
thaysgama <thays.s.gama@gmail.com>
2021-12-06 11:55:30 -0300

    adiciona categorias

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,9 @@
-INSERT INTO categories (name) VALUES ('suculentas');
+INSERT INTO categories (name) VALUES
+    ('suculentas'),
+    ('cactos'),
+    ('samambaias'),
+    ('flores'),
+    ('jardinagem');
 
 INSERT INTO products (title, price, description, image, category_id) VALUES
     ('Rosa-do-deserto', 15.0, '(Adenium obesum)', '/caminho', 1),
